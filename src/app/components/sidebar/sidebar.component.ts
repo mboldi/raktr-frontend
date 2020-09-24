3cfe9e665fcd7e8cf2e2057867ff72547bc2ab1a
Márta Boldizsár <marta.boldi13@gmail.com>
2020-09-24 12:48:32 +0200

    initial commit with some adjustments to template

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,41 @@
+import {Component, OnInit} from '@angular/core';
+
+declare const $: any;
+
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+
+export const ROUTES: RouteInfo[] = [
+    {path: '/overview', title: 'Áttekintés', icon: 'dashboard', class: ''},
+    //{path: '/dashboard', title: 'Dashboard', icon: 'dashboard', class: ''},
+    //{path: '/user-profile', title: 'User Profile', icon: 'person', class: ''},
+    {path: '/devices', title: 'Eszközök kezelése', icon: 'sd_storage', class: ''}, //content_paste
+    //{path: '/typography', title: 'Typography', icon: 'library_books', class: ''},
+    //{path: '/icons', title: 'Icons', icon: 'bubble_chart', class: ''},
+    //{path: '/maps', title: 'Maps', icon: 'location_on', class: ''},
+    //{ path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' }
+];
+
+@Component({
+    selector: 'app-sidebar',
+    templateUrl: './sidebar.component.html',
+    styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+    menuItems: any[];
+
+    constructor() {
+    }
+
+    ngOnInit() {
+        this.menuItems = ROUTES.filter(menuItem => menuItem);
+    }
+
+    isMobileMenu() {
+        return $(window).width() <= 991;
+    };
+}
---
+++ src/app/devices/devices.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-list',
+  templateUrl: './devices.component.html',
+  styleUrls: ['./devices.component.css']
+})
+export class DevicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
