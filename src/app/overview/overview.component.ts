70d27852b6c47c98c55a9faae2bb71896cdae29d
Márta Boldizsár <marta.boldi13@gmail.com>
2020-10-17 22:25:16 +0200

    overview data from services

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Title} from '@angular/platform-browser';
+import {RentService} from '../services/rent.service';
+import {DeviceService} from '../services/device.service';
+import {Rent} from '../model/Rent';
+import {Device} from '../model/Device';
 
 @Component({
     selector: 'app-overview',
@@ -9,11 +13,24 @@ import {Title} from '@angular/platform-browser';
 })
 export class OverviewComponent implements OnInit {
 
-    constructor(private title: Title) {
+    rents: Rent[] = [];
+    devices: Device[] = [];
+
+    constructor(private title: Title, private rentService: RentService, private deviceService: DeviceService) {
         this.title.setTitle('Raktr - Áttekintés');
     }
 
     ngOnInit(): void {
+        this.rentService.getRents().subscribe(rents => this.rents = rents);
+        this.deviceService.getDevices().subscribe(devices => this.devices = devices);
+    }
+
+    getNumOfActiveRents(): number {
+        return this.rents.filter(rent => rent.actBackDate !== '').length;
+    }
+
+    activeRents(): Rent[] {
+        return this.rents.filter(rent => rent.actBackDate !== '');
     }
 
 }
