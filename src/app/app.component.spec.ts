4fd32e1565dd53794e22d286a397041a5401ec5a
Márta Boldizsár <marta.boldi13@gmail.com>
2020-11-30 17:17:02 +0100

    testing setup start

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,32 +1,40 @@
-import { TestBed, async } from '@angular/core/testing';
+import {async, TestBed} from '@angular/core/testing';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {DeviceService} from './services/device.service';
+import {MockDeviceService} from './services/mock-device.service';
+import {AuthService} from './services/auth.service';
+import {MockAuthService} from './services/mock-auth.service';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                AppComponent
+            ],
+            providers: [
+                {provide: DeviceService, useClass: MockDeviceService},
+                {provide: AuthService, useClass: MockAuthService}
+            ]
+        }).compileComponents();
+    }));
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+    it('should create the app', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    }));
+/*it(`should have as title 'Raktr - Áttekintés'`, async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        console.log(app);
+        expect(app.title).toEqual('Raktr - Áttintés');
+    }));
 
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  }));
+    it('should render title in a h1 tag', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    }));*/
 });
---
+++ src/app/services/mock-auth.service.ts
@@ -0,0 +1,47 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {UserService} from './user.service';
+// @ts-ignore
+import moment = require('moment');
+
+@Injectable({
+    providedIn: 'root'
+})
+export class MockAuthService {
+
+    constructor(private http: HttpClient,
+                private userService: UserService) {
+    }
+
+    // @ts-ignore
+    login(username: string, password: string) {
+        this.setSession(JSON.parse('{"body": {"expiresAt": "1111.11.11.", "token": "asdassd.asdasd.asdasddsa"}}'), username);
+        return true;
+    }
+
+    setSession(authResult, username) {
+        localStorage.setItem('id_token', authResult.body.token);
+        localStorage.setItem('expires_at', authResult.body.expiresAt);
+        localStorage.setItem('username', username);
+    }
+
+    logout() {
+        localStorage.removeItem('id_token');
+        localStorage.removeItem('username');
+        localStorage.removeItem('expires_at');
+    }
+
+    public isLoggedIn() {
+        return true;
+    }
+
+    isLoggedOut() {
+        return !this.isLoggedIn();
+    }
+
+    getExpiration() {
+        const expiration = localStorage.getItem('expires_at');
+        const expiresAt = JSON.parse(expiration);
+        return moment(expiresAt);
+    }
+}
