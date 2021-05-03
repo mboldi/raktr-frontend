7cbfd9b62c751988e6399ca1e3c97770c0b6d45c
Boldizsár Márta <marta.boldi13@gmail.com>
2021-05-03 23:10:08 +0200

    added admin role checking

--- src/app/model/User.ts
+++ src/app/model/User.ts
@@ -11,9 +11,17 @@ export class User {
     token?: string;
 
     static isStudioMember(user: User): boolean {
+        return this.checkRole(user, 'ROLE_Stúdiós');
+    }
+
+    static isAdmin(user: User): boolean {
+        return this.checkRole(user, 'ROLE_ADMIN');
+    }
+
+    static checkRole(user: User, wantedRole: string): boolean {
         for (let i = 0; i < user.roles.length; i++) {
             const role = user.roles[i];
-            if (role.roleName === 'ROLE_Stúdiós') {
+            if (role.roleName === wantedRole) {
                 return true;
             }
         }
