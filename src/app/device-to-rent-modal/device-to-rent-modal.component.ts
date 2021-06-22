28ced948d0132d0c220760bcfab2143866498b31
Márta Boldizsár <marta.boldi13@gmail.com>
2021-06-22 21:40:41 +0200

    device amount not updated on escape key when adding device to rent with multiplicity

--- src/app/device-to-rent-modal/device-to-rent-modal.component.ts
+++ src/app/device-to-rent-modal/device-to-rent-modal.component.ts
@@ -1,24 +1,31 @@
-import {Component, Input, OnInit, Output} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Device} from '../model/Device';
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import {FormControl} from '@angular/forms';
 
 @Component({
-  selector: 'app-device-to-rent-modal',
-  templateUrl: './device-to-rent-modal.component.html',
-  styleUrls: ['./device-to-rent-modal.component.css']
+    selector: 'app-device-to-rent-modal',
+    templateUrl: './device-to-rent-modal.component.html',
+    styleUrls: ['./device-to-rent-modal.component.css']
 })
 export class DeviceToRentModalComponent implements OnInit {
-  @Input() device: Device;
-  amountFormControl = new FormControl();
+    @Input() device: Device;
+    amountFormControl = new FormControl();
 
-  constructor(public activeModal: NgbActiveModal) { }
+    constructor(public activeModal: NgbActiveModal) {
+    }
 
-  ngOnInit(): void {
-    this.amountFormControl.setValue(1);
-  }
+    ngOnInit(): void {
+        this.amountFormControl.setValue(1);
+    }
 
-  finish() {
-    this.activeModal.dismiss(this.amountFormControl.value);
-  }
+    finish() {
+        this.activeModal.dismiss(this.amountFormControl.value);
+    }
+
+    keydown($event: KeyboardEvent) {
+        if ($event.key === 'Escape') {
+            this.activeModal.dismiss(null);
+        }
+    }
 }
